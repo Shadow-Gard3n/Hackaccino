a1d2769fa794e04cdefc026b911595b66052a032
Aryan Gahlot <aryangahlot194750@gmail.com>
2025-03-27 01:26:07 +0530

    Corrected Logout Button

--- service/firebase_config.py
+++ service/firebase_config.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials, firestore
 import os
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("D:\\Coding\\web with python\\FastApi\\project\\serviceDatabaseKey.json")  # Path to your Firebase Admin SDK JSON
+    cred = credentials.Certificate("D:\\Coding\\web with python\\FastApi\\project\\serviceDatabaseKey.json")  
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
